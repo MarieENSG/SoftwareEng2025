c3be7e9b97fc9f51b70250fd9dd45563b91ee1a6
MarieENSG <marie.moulard88@gmail.com>
2025-09-18 11:15:59 +0200

    set project version and specify cpp version

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,17 @@
-# include <stdio.h>
+#include <iostream>
+#include <HelloProjConfig.h>
+
+void hello_world() {
+ std::cout << "Hello World!\n";
+}
+
+void print_version(){
+ std::cout<< "project version is " << HelloProj_VERSION_MAJOR  << "."
+ << HelloProj_VERSION_MINOR << std::endl;
+}
 
 int main()
- {
-	printf("Hello, World !");
-	
-	return 0;
+{
+ hello_world();
+ print_version();
 }
