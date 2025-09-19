3d502b70d911bcbbcc59be768c7ef9207da75bdd
MarieENSG <moulard6u@etu.univ-lorraine.fr>
2025-09-19 11:33:46 +0200

    add test for hello and print_version

--- tests/test-hello.cpp
+++ tests/test-hello.cpp
@@ -2,7 +2,6 @@
 
 int main() {
 	hello_world();
-	print_version();
- 
+	
 	return 0;
 }
--- tests/test-version.cpp
+++ tests/test-version.cpp
@@ -1,8 +1,7 @@
 #include <basic/message.h>
 
 int main() {
-	hello_world();
 	print_version();
- 
+	
 	return 0;
 }
