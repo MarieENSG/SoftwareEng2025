96871c6e9b7a64e72a6a9b36d8203b9c9cbd8a54
MarieENSG <marie.moulard88@gmail.com>
2025-09-23 11:30:43 +0200

    reformating

--- src/basic/message.cpp
+++ src/basic/message.cpp
@@ -1,6 +1,6 @@
-# include <iostream>
-# include <HelloProjConfig.h>
-# include <basic/message.h>
+#include <iostream>
+#include <HelloProjConfig.h>
+#include <basic/message.h>
 
 void hello_world() {
 	std::cout << "Hello World" << std::endl;
